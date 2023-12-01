8bcd3f870b2d1c7f7853fd6c8b837490af342e88
cendanaaul <cendanaputria@gmail.com>
2023-12-01 20:26:11 +0700

    Initial commit

---
+++ app/src/main/java/com/cencen/bloommate/data/SplashScreenData.kt
@@ -0,0 +1,11 @@
+package com.cencen.bloommate.data
+
+import androidx.compose.ui.graphics.Color
+
+data class SplashScreenData(
+    val image: Int,
+    val backgroundColor: Color,
+    val mainColor: Color,
+    val headText: String,
+    val subText: String,
+)
